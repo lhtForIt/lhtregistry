3a097297940c1feaced80e55f037c4145bdad269
Leo <lhtshent@gmail.com>
2024-04-22 18:26:56 +0800

    add health check config

--- src/main/java/io/github/lhtforit/lhtregistry/LhtConfig.java
+++ src/main/java/io/github/lhtforit/lhtregistry/LhtConfig.java
@@ -1,7 +1,10 @@
 package io.github.lhtforit.lhtregistry;
 
+import io.github.lhtforit.lhtregistry.healthChecker.HealthChecker;
+import io.github.lhtforit.lhtregistry.healthChecker.LhtHealthChecker;
 import io.github.lhtforit.lhtregistry.service.LhtRegistryService;
 import io.github.lhtforit.lhtregistry.service.RegistryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +20,11 @@ public class LhtConfig {
     public RegistryService registryService()
     {
         return new LhtRegistryService();
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public HealthChecker healthChecker(RegistryService registryService){
+        return new LhtHealthChecker(registryService);
     }
 
 
